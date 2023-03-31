19ea20e8cc92581d36a4c59e39b17899b286f2ca
EdaAltunay <altunay.eda1984@gmail.com>
2023-03-31 10:01:13 +0300

    31mart2023

---
+++ src/Practice/P01_forLoop.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class P01_forLoop {
+    public static void main(String[] args) {
+        /*        Kullanicidan pozitif bir tamsayi alin, 1’den girilen sayiya kadar
+                  (girilen sayi dahil) 7 ila bolunebilen sayilari yazdirin.
+                */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lutfen bir tamsayi giriniz: ");
+        int girilenSayi = scan.nextInt();
+        for (int i = 0; i <= girilenSayi; i++) {
+            if (i % 7 == 0) {
+            System.out.print(i + "-");}
+        }
+        System.out.println("");
+        System.err.println("===========   İSLEM TAMAMLANDİ   ==========");
+
+
+    }
+
+}
---
+++ src/Practice/P04_forLoop.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class P04_forLoop {
+    public static void main(String[] args) {
+      //  Kullanicidan bir String isteyin ve String'i tersine cevirip kaydedin.
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
