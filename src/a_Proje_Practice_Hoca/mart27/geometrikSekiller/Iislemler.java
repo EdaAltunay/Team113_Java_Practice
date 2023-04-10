6e750dee226391789448b80da6ab973569533ffb
EdaAltunay <altunay.eda1984@gmail.com>
2023-04-10 11:06:20 +0300

    10Nisan2023

--- src/a_Proje_Practice_Hoca/mart27/geometrikSekiller/Iislemler.java
+++
@@ -1,6 +0,0 @@
-package a_Proje_Practice_Hoca.mart27.geometrikSekiller;
-
-public interface Iislemler {
-    void alan();
-    void cevre();
-}
---
+++ src/a_Proje_Practice_Hoca/mart27/mart27/geometrikSekiller/Iislemler.java
@@ -0,0 +1,6 @@
+package a_Proje_Practice_Hoca.mart27.mart27.geometrikSekiller;
+
+public interface Iislemler {
+    void alan();
+    void cevre();
+}
